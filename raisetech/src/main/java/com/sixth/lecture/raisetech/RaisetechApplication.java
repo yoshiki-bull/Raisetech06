9002b04ed1121fc14705d6eb6e262afc436db789
yoshiki-bull <dnttz118@outlook.jp>
2023-01-07 15:45:53 +0900

    デフォルト

---
+++ raisetech/src/main/java/com/sixth/lecture/raisetech/RaisetechApplication.java
@@ -0,0 +1,13 @@
+package com.sixth.lecture.raisetech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaisetechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaisetechApplication.class, args);
+	}
+
+}
---
+++ raisetech/src/test/java/com/sixth/lecture/raisetech/RaisetechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sixth.lecture.raisetech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaisetechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
